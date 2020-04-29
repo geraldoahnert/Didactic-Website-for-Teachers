58b5db5f5bf60314e4ad94e78ad91d1689c23ae9
i386angel <geraldoahnert@gmail.com>
2020-04-29 17:25:19 -0300

    Repaginando todo o projeto, adicionando novas práticas e retirando algumas vulnerabilidades.

--- crud/cadastro.php
+++ crud/cadastro.php
@@ -1,87 +1,57 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>DwT - Login</title>
-	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-
-
-
-	<div class="container" style="margin: 100px auto; width: 300px;">
-		
-		<form class="form-group" action="_cadastrar_usuario.php" method="post">
+	<head>
+		<meta charset="utf-8">
+		<title>DwT - Login</title>
+		<script src="https://kit.fontawesome.com/7afebb249a.js" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div class="container" style="margin: 100px auto; width: 300px;">
+			
+			<form class="form-group" action="_cadastrar_usuario.php" method="post">
 				<div class="form-group">
 					<label><i class="fas fa-user"></i></label>
 					<input type="text" name="userEmail" class="form-control" required="true" placeholder="Email">
 				</div>
-
 				<div class="form-group" style="margin-top: 5%">
 					<label><i class="fas fa-lock"></i></label>
 					<input type="password" name="userPasswd" class="form-control" required="true" placeholder="Senha">
 					<input style="margin-top: 5%" type="password" name="passwordUserConfirm" class="form-control" required="true" placeholder="Repita">
 				</div>
-
-
 				<div class ="form-group">
-
 					<label><i class="fas fa-project-diagram"></i></label>
 					<select name="userLevel" class="form-control">
 						<option value="1">Aluno</option>}
 						<option value="2">Professor</option>}
 					</select>
-
 				</div>
-
-
 				<div class="form-group">
 					<label><i class="fas fa-graduation-cap"></i></label>
 					<select class="form-control" name="userCategoria" required="true">
-
-							<?php
-
-							include '_conexao.php';
-
-							$sql = "SELECT * FROM categoria order by nome_categoria ASC";
-							$buscar = mysqli_query($conexao, $sql);
-
-
-							while($array = mysqli_fetch_array($buscar)){
-
-
-								$id_categoria = $array['id_categoria'];
-								$nome_categoria = $array['nome_categoria'];
-							
-
-							?>
-
-			     	 	<option><?php echo $nome_categoria ?></option>
-			     	 	
-				      	<?php } ?>
-
-
-		    		</select>
-		  		</div>
-
+						<?php
+						include '_conexao.php';
+						$sql = "SELECT * FROM categoria order by nome_categoria ASC";
+						$buscar = mysqli_query($conexao, $sql);
+						while($array = mysqli_fetch_array($buscar)){
+							$id_categoria = $array['id_categoria'];
+							$nome_categoria = $array['nome_categoria'];
+						
+						?>
+						<option><?php echo $nome_categoria ?></option>
+						
+						<?php } ?>
+					</select>
+				</div>
 				<div id="botoesDireita" class="form-group">
 					<a href="index.php" type="submit" class="btn btn-dark btn-sm">Entrar</a>
 					<button type="submit" class="btn btn-info btn-sm">Registrar</button>
-				</div> 
-		</form>
-
-	</div>	
-
-
-
-
-
-
-
-	<script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
-	<script type="text/javascript" src="node_modules/jquery/dist/jquery.js"></script>
-	<script type="text/javascript" src="node_modules/popper.js/dist/popper.js"></script>	
-	<script src="https://kit.fontawesome.com/7afebb249a.js" crossorigin="anonymous"></script>
-</body>
+				</div>
+			</form>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	</body>
 </html>
--- crud/index.php
+++ crud/index.php
@@ -1,38 +1,42 @@
+<?php
+require_once 'login_action.php';
+?>
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Tela de Login</title>
-	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-
-	<div class="container" style="margin: 200px auto; width: 300px;">
-		<form action="_informacoes.php" method="post">
-
-			<div class="form-group">
-				<label><i class="fas fa-user"></i></label>
-				<input type="text" name="usuario" class="form-control" required="true" placeholder="Email">
-			</div>
-
-			<div class="form-group">
-				<label><i class="fas fa-lock"></i></label>
-				<input type="password" name="senha" class="form-control" required="true" placeholder="Senha">
-			</div>
-
-			<div id="botoesDireita">
-				<a href="cadastro.php" type="submit" class="btn btn-dark btn-sm">Registrar</a>
-				<button type="submit" class="btn btn-info btn-sm">Entrar</button>
-			</div> 
-
-		</form>
-	</div>
-
-	<script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
-	<script type="text/javascript" src="node_modules/jquery/dist/jquery.js"></script>
-	<script type="text/javascript" src="node_modules/popper.js/dist/popper.js"></script>	
-	<script src="https://kit.fontawesome.com/7afebb249a.js" crossorigin="anonymous"></script>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Tela de Login</title>
+		<script src="https://kit.fontawesome.com/7afebb249a.js" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div class="container" style="margin: 200px auto; width: 300px;">
+			<form action="login_action.php" method="post">
+				<div class="form-group">
+					<label><i class="fas fa-user"></i></label>
+					<input type="text" name="login" class="form-control" required="true" placeholder="Email">
+				</div>
+				<div class="form-group">
+					<label><i class="fas fa-lock"></i></label>
+					<input type="password" name="password" class="form-control" required="true" placeholder="Senha">
+				</div>
+				<div id="botoesDireita">
+					<a href="cadastro.php" type="submit" class="btn btn-dark btn-sm">Registrar</a>
+					<button type="submit" name="btn-login" class="btn btn-info btn-sm">Entrar</button>
+				</div>
+				<?php
+					if (!empty($erros)) {
+						foreach ($erros as $erro) {
+							echo "<li> $erro </li>";
+						}
+					}
+				?>
+			</form>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	</body>
 </html>
